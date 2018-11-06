9bf77568f459b703f605bdbd9c37199d86c52026
Mattia Primavera <mattia@norbertapp.com>
2018-11-07 00:49:51 +0100

    Token interceptor: using getToken from authService

--- interceptors/token-interceptor.ts
+++ interceptors/token-interceptor.ts
@@ -1,29 +1,20 @@
-import { PreferencesService } from './../../src/providers/app-preferences.service';
 import { Injectable } from '@angular/core';
 import {
-    HttpRequest,
-    HttpResponse,
-    HttpHandler,
-    HttpEvent,
-    HttpInterceptor,
-    HttpErrorResponse,
-    HttpSentEvent,
-    HttpHeaderResponse,
-    HttpProgressEvent,
-    HttpUserEvent
-  } from '@angular/common/http';
-  import { Observable } from 'rxjs/Observable';
-  import 'rxjs/add/operator/do';
-  import Rx from 'rxjs/Rx';
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
 import { AuthService } from '../providers/auth-service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-    constructor(private preferencesService: PreferencesService){}
+    constructor(private authService: AuthService){}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        let userProfile = this.preferencesService.getUserProfile();
-        let token = userProfile ? userProfile.token : null;
+        let token = this.authService.getToken();
         console.log('INTERCEPTOR: token -> ' + token);
         if(token == null) {
             return next.handle(request);
